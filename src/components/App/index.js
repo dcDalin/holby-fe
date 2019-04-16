020b89687f817d303da1558d0f66f263c03cf173
Dalin  Oluoch <mcdalinoluoch@gmail.com>
2019-04-17 00:33:48 +0300

    [Chore] Uninstall custom theming and styled components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-import { GlobalStyle, Theme } from '../../theme';
-
-const Heading = styled.h1`
-  ${({ theme }) => theme.mobile`
-      color: red;
-   `}
-`;
 
 const App = () => {
-  return (
-    <Theme>
-      <>
-        <Heading>Hello</Heading>
-        <GlobalStyle />
-      </>
-    </Theme>
-  );
+  return <h1>App</h1>;
 };
 
 export default App;
