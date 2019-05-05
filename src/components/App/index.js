f4ec8a51fcde668bcd6716dca920b5bd8b9651fe
Dalin  Oluoch <mcdalinoluoch@gmail.com>
2019-05-06 00:38:04 +0300

    [Chore] UI Update requests

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,31 @@
+import { Grommet } from 'grommet';
 import React from 'react';
-import { GlobalStyle, Theme } from '../../theme';
 import Router from '../Router';
+
+const theme = {
+  global: {
+    colors: {
+      brand: 'rgba(228, 43, 25, 1.0)',
+      fadeGreen: 'rgba(17, 119, 85, 0.1)',
+    },
+    font: {
+      family: 'Montserrat',
+    },
+    anchor: {
+      fontWeight: 600,
+      hover: {
+        textDecoration: 'underline dotted',
+        fontWeight: 600,
+      },
+    },
+  },
+};
 
 const App = () => {
   return (
-    <Theme>
-      <>
-        <Router />
-        <GlobalStyle />
-      </>
-    </Theme>
+    <Grommet theme={theme}>
+      <Router />
+    </Grommet>
   );
 };
 export default App;
---
+++ src/components/Header/LogoItems/index.js
@@ -0,0 +1,3 @@
+import LogoItems from './LogoItems';
+
+export default LogoItems;
